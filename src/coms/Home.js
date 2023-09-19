55756191ff5144c677f179387c3547558ba7cc45
宁静 <846526253@qq.com>
2023-09-19 16:04:29 +0800

    css 配置

--- src/coms/Home.js
+++ src/coms/Home.js
@@ -1,19 +1,31 @@
 import React from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { decrement, increment } from '../store/countSlice'
-
+import style from "./Home.module.css"
+import frog from './asse/frog.png';
 
 function Home() {
-    const count = useSelector((state) => state.counter.value)
-    const dispatch = useDispatch()
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
 
-    return (
-      <div>
-        <div onClick={() => dispatch(increment())}>increment</div>
-        <span>{count}</span>
-        <div onClick={() => dispatch(decrement())}>decrement</div>
+  return (
+    <div className={style.box}>
+      <div className={style.header}>
+        <div className={style.tree}></div>
+        <div className={style.mountain}></div>
       </div>
-    )
+
+      <div className={style.river}>
+        <div className={style['heye-box']} id="myButton">Click me</div>
+      </div>
+
+      <div className={style.platform}>
+        <div className={style.frog} >
+          <img src={frog} alt="My Image" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,14 @@ module.exports = {
                 test: /\.js$/,
                 use: "babel-loader",
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /.(png|jpe?g|gif)$/i,
+                use: ['file-loader']
             }
         ]
     },
